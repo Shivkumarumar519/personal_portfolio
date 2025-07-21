7aeb7f03d8d3288cc2b999e96b6a3952a1c383e7
shiv kumar umar <143341202+Shivkumarumar519@users.noreply.github.com>
2025-07-21 10:55:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
   }
 
 
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Change button text based on current mode
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
+
