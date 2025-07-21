cb15b2db78a76dead68a02ea8a22c3e3ccc362c0
shiv kumar umar <143341202+Shivkumarumar519@users.noreply.github.com>
2025-07-21 11:09:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,4 @@ function toggleMenu() {
   }
 
 
-const toggleBtn = document.getElementById("theme-toggle");
-
-// Load theme from localStorage
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
-  toggleBtn.textContent = "☀️ Light Mode";
-}
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  const isDark = document.body.classList.contains("dark");
-
-  // Save theme
-  localStorage.setItem("theme", isDark ? "dark" : "light");
-
-  // Update button text
-  toggleBtn.textContent = isDark ? "☀️ Light Mode" : "🌙 Dark Mode";
-});
-
 
