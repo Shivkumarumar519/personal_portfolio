e4851cacdfca0e0455d53d08ad99029fef22ab6c
shiv kumar umar <143341202+Shivkumarumar519@users.noreply.github.com>
2025-07-21 10:45:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+
+<script>
+  const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    // Change button text/icon
+    if (document.body.classList.contains("dark")) {
+      toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+      toggleBtn.textContent = "🌙 Dark Mode";
+    }
+  });
+</script>
